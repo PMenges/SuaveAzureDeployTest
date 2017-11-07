a55e72fb3a1a6ac45963d5395137b22484060e0c
Paul Menges <paul.menges@freenet.de>
2017-11-07 15:12:15 +0100

    adding azure.build.fsx + deployment files

---
+++ build.azure.fsx
@@ -0,0 +1,26 @@
+﻿#r "packages/Suave/lib/net40/Suave.dll"
+#r "packages/FAKE/tools/FakeLib.dll"
+#load "app.fsx"
+open App
+open Fake
+
+open System
+open System.Net
+open System.IO
+open Suave
+open Suave.Web
+open Suave.Successful
+open Suave.Http
+
+
+let serverConfig =
+    let port = Sockets.Port.Parse (getBuildParam "port")
+    { defaultConfig with
+        homeFolder = Some __SOURCE_DIRECTORY__
+        bindings = [ HttpBinding.create HTTP IPAddress.Loopback port ] }
+
+Target "run" (fun _ ->
+    startWebServer serverConfig app
+)
+
+RunTargetOrDefault "run"
