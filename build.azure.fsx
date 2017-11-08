161a9b8632760f74c8c4bbfd0a08c1b9a2c77e27
Paul Menges <paul.menges@freenet.de>
2017-11-08 16:21:02 +0100

    Updated web.config / deploy cmd

--- build.azure.fsx
+++ build.azure.fsx
@@ -1,5 +1,6 @@
 ﻿#r "packages/Suave/lib/net40/Suave.dll"
 #r "packages/FAKE/tools/FakeLib.dll"
+
 #load "app.fsx"
 open App
 open Fake
@@ -16,18 +17,13 @@ open Suave.Http
 open Suave.Operators
 open Suave.Filters
 
-let serverConfig =
-    let port = System.Environment.GetEnvironmentVariable("PORT")
-    let ip127  = IPAddress.Parse("127.0.0.1")
-    let ipZero = IPAddress.Parse("0.0.0.0")
+Environment.CurrentDirectory <- __SOURCE_DIRECTORY__
 
+let serverConfig =
+    let port = Sockets.Port.Parse <| getBuildParamOrDefault "port" "8083"
     { defaultConfig with
         homeFolder = Some __SOURCE_DIRECTORY__
-        bindings=[ (if port = null then HttpBinding.create HTTP ip127 (uint16 8080)
-                    else HttpBinding.create HTTP ipZero (uint16 port)) ] }
+        bindings = [ HttpBinding.create HTTP IPAddress.Loopback port ] }
 
-Target "run" (fun _ ->
-    startWebServer serverConfig app
-)
-
-RunTargetOrDefault "run"
+startWebServer serverConfig app
+ 
