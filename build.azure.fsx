c268a51e1b1cbd65314a7054521c0d1ba0952f72
Paul Menges <paul.menges@freenet.de>
2017-11-08 13:16:32 +0100

    Fixes

--- build.azure.fsx
+++ build.azure.fsx
@@ -6,18 +6,25 @@ open Fake
 
 open System
 open System.Net
+open System.Threading.Tasks
 open System.IO
+
 open Suave
 open Suave.Web
 open Suave.Successful
 open Suave.Http
-
+open Suave.Operators
+open Suave.Filters
 
 let serverConfig =
-    let port = Sockets.Port.Parse (getBuildParamOrDefault "port" "8083")
+    let port = System.Environment.GetEnvironmentVariable("PORT")
+    let ip127  = IPAddress.Parse("127.0.0.1")
+    let ipZero = IPAddress.Parse("0.0.0.0")
+
     { defaultConfig with
         homeFolder = Some __SOURCE_DIRECTORY__
-        bindings = [ HttpBinding.create HTTP IPAddress.Loopback port ] }
+        bindings=[ (if port = null then HttpBinding.create HTTP ip127 (uint16 8080)
+                    else HttpBinding.create HTTP ipZero (uint16 port)) ] }
 
 Target "run" (fun _ ->
     startWebServer serverConfig app
