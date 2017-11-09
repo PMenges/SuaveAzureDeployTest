2c02433af09bcf4f919c5dc0259a3087e918bdb7
Paul Menges <paul.menges@freenet.de>
2017-11-09 14:38:02 +0100

    Updated deploy.cmd

--- deploy.cmd
+++ deploy.cmd
@@ -1,14 +1,13 @@
 @echo off
 cls
 
-.paket\paket.bootstrapper.exe
-if errorlevel 1 (
-  exit /b %errorlevel%
-)
-
 .paket\paket.exe restore
 if errorlevel 1 (
   exit /b %errorlevel%
 )
 
-xcopy %DEPLOYMENT_SOURCE%\*.* %DEPLOYMENT_TARGET%\ /Y /s 
+IF NOT EXIST build.azure.fsx (
+  .paket\paket.exe update
+  packages\build\FAKE\tools\FAKE.exe init.fsx
+)
+packages\build\FAKE\tools\FAKE.exe build.azure.fsx %*
